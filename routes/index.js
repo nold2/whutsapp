64812bc82333102667e514645662060fc977251b
Arnold Sebastian <arnold@danacita.com>
2019-03-23 18:42:21 +0700

    added two new routes the default route and the new route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const db = require("rethinkdb");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let connection;
+db.connect({ host: "localhost", port: 28015, db: "test" }).then(conn => {
+  connection = conn;
+});
+
+router.get("/", async (req, res) => {
+  const posts = await db
+    .table("posts")
+    .orderBy(db.desc("date"))
+    .run(connection)
+    .then(cursor => cursor.toArray());
+
+  res.render("index", { posts });
+});
+
+router.get("/new", (req, res) => {
+  res.render("new");
+});
+
+router.post("/new", async (req, res) => {
+  const post = {
+    title: req.body.title,
+    content: req.body.content,
+    date: new Date()
+  };
+
+  db.table("posts")
+    .insert(post)
+    .run(connection)
+    .then(() => res.redirect("/"));
 });
 
 module.exports = router;
